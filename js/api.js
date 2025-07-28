fb246c42b5022d35391ea13362d496d7e70845a1
Seba7github <164034780+Seba7github@users.noreply.github.com>
2025-07-28 18:48:43 -0400

    mod: index

---
+++ js/api.js
@@ -0,0 +1,34 @@
+// PROD
+base_url = "https://gestor-it.com/libroquejas/api/v1.0/"
+// DEV
+//base_url = "http://localhost/API-RECLAMOS/api/v1.0/"
+
+
+async function get_users(user,pass){
+    url = base_url + "usuarios.php?usuario=" + user+ "&password=" + pass;
+    let response = await fetch(url);
+    let data = await response.json();
+    return data; 
+}
+
+async function post_reclamo(reclamo){
+    url = base_url
+    url = base_url + "reclamos.php";
+    var data1 = {reclamo: reclamo};
+    options={
+        method:"POST",
+        body: JSON.stringify(data1)
+    };
+    alert("URL: " + url);
+    let response = await fetch(url, options);
+    let data = await response.json();
+    alert(JSON.stringify(data));
+    return data; 
+}
+
+async function login_usuario(usuario, password){
+    url = base_url + "usuarios.php?usuario=" + usuario + "&password=" + password;
+    let response = await fetch(url);
+    let data = await response.json();
+    return data; 
+}
